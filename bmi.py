5d20b4d9533c73eba615611eba40048b209e1eb4
NaftaliRozenberg <rwznty825@gmail.com>
2025-02-10 10:24:35 +0200

    Add files via upload

---
+++ bmi.py
@@ -0,0 +1,14 @@
+weight=float(input("what is your weight? "))
+height=float(input("what is your height? "))
+BMI=weight / height ** 2
+if (BMI   <18):
+	print("underweight")
+if (BMI >=18 and BMI <=25):
+	print("normal weignt")
+if (BMI >25 and BMI <=30):
+	print("overweight")
+if (BMI >30 and BMI <=35):
+	print("grade 2 overweight")
+if (BMI >35):
+	print("danger to life")
+    
